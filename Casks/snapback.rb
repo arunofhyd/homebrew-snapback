bd674aaedf075b1e1bb319157c540756e652d35b
arunofhyd <104165035+arunofhyd@users.noreply.github.com>
2026-07-15 09:25:10 +0530

    Update snapback.rb

--- Casks/snapback.rb
+++ Casks/snapback.rb
@@ -1,6 +1,6 @@
 cask "snapback" do
   version "1.0.0"
-  sha256 "ffe99ae4b45025da635531353c1873c1b4cb0263b8c792b3f9d1d82044f1cee6" # Make sure to put the actual shasum output here!
+  sha256 "5facc7a6d16023bb745fa48bb548100dfe997db2b2ce4ac81e596fd11d1067df" # Make sure to put the actual shasum output here!
 
   url "https://github.com/arunofhyd/SnapBack/releases/download/v#{version}/SnapBack.zip"
   name "SnapBack"
